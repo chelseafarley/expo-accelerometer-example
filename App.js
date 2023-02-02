9af9f98e5559c1b3ef985fa839e17947b0cfc94c
Chelsea <chelsea@tripwiretech.com>
2023-02-02 20:07:20 +1300

    Accelerometer

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { useState, useEffect } from 'react';
+import { Accelerometer } from 'expo-sensors';
+
+// npx expo install expo-sensors
 
 export default function App() {
+  const [ { x, y, z}, setData] = useState({ x: 0, y: 0, z: 0 });
+  
+  useEffect(() => {
+    const subscription = Accelerometer.addListener(setData);
+    return () => subscription.remove();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>x: {x}</Text>
+      <Text>y: {y}</Text>
+      <Text>z: {z}</Text>
+      <Button title='Slow' onPress={() => Accelerometer.setUpdateInterval(2000)} />
+      <Button title='Fast' onPress={() => Accelerometer.setUpdateInterval(50)} />
       <StatusBar style="auto" />
     </View>
   );
